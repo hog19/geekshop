64941df7158425e793e5101764ef0a1ab6fca397
hog19 <hausofgrisha@gmail.com>
2022-05-26 00:53:52 +0300

    Lesson 2

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,7 +20,14 @@ from mainapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.index),
-    path('products/', views.products),
-    path('contact/', views.contact),
+    path('', views.index, name='index'),
+
+    path('products/', views.products, name='products'),
+    path('products/classic/', views.products, name='products_classic'),
+    path('products/all/', views.products, name='products_all'),
+    path('products/home/', views.products, name='products_home'),
+    path('products/office/', views.products, name='products_office'),
+    path('products/modern/', views.products, name='products_modern'),
+
+    path('contact/', views.contact, name='contact'),
 ]
